fac582da66fedd736de3bc56b27bd55b080b4595
Alice Wyan <alice@finitud.org>
2012-11-11 17:20:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e21479583ba72527830ff873243131c7ef061a748663458d8502fbd17335a6dfbc6d833bf7c0993770679b2e839f9ffc73aa683dcee82ce15a159040fa54c235'
