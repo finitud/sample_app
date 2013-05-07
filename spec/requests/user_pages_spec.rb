9e11b7cb9f3f150b12e8270d5636649c182c396b
Alice Wyan <alice@finitud.org>
2013-05-07 14:04:51 +0100

    Add user index page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,13 +4,35 @@ describe "UserPages" do
 
   subject { page }
 
+  describe "index page" do
+
+    let(:user) { FactoryGirl.create :user }
+
+    before do
+      sign_in user
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Rob", email: "rob@example.com")
+      FactoryGirl.create(:user, name: "Ted", email: "ted@example.com")
+      FactoryGirl.create(:user, name: "Jon", email: "jon@example.com")
+      visit users_path
+    end
+
+    it { should have_selector 'h1',    text: 'All users'}
+    it { should have_selector 'title', text: 'All users'}
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector 'li>a', text: user.name
+      end
+    end
+  end
+
   describe "sign up page" do
 
     before { visit signup_path }
 
-    it { should have_selector('h1', :text => 'Sign up') }
+    it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
-
   end
 
   describe "profile page" do
@@ -21,7 +43,6 @@ describe "UserPages" do
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
-
   end
 
   describe "signup" do
@@ -66,6 +87,44 @@ describe "UserPages" do
         it { should have_link 'Sign out' }
       end
     end
+  end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in(user)
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector 'h1',    text: 'Update profile' }
+      it { should have_selector 'title', text: 'Edit user' }
+      it { should have_link 'change', href: 'http://gravatar.com/emails' }
+    end
+
+    describe "with invalid information" do
+      before { click_button 'Save changes' }
+      it { should have_content 'error' }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New name" }
+      let(:new_email) { "new@example.com" }
+
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector 'title', text: new_name }
+      it { should have_link 'Sign out', href: signout_path }
+      it { should_not have_link 'Sign in', href: signin_path }
+      it { should have_selector 'div.alert.alert-success' }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
+    end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,11 @@
 include ApplicationHelper
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# When not using Capybara (because we're issuing a HTTP request directly,
+	# for example), we need to set the remember token manually
+	cookies[:remember_token] = user.remember_token
+end
